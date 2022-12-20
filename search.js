0756ddbbc2d676d81dfcc205d2dd0f9acef74126
Seunghwan <kim03208@naver.com>
2022-12-20 23:05:48 +0900

    add template
    https://developers.naver.com/docs/serviceapi/search/blog/blog.md#node-js

---
+++ search.js
@@ -0,0 +1,26 @@
+// 네이버 검색 API 예제 - 블로그 검색
+const express = require("express");
+const app = express();
+const client_id = "YOUR_CLIENT_ID";
+const client_secret = "YOUR_CLIENT_SECRET";
+app.get("/search/blog", function (req, res) {
+  const api_url = "https://openapi.naver.com/v1/search/cafe?query=" + encodeURI(req.query.query); // JSON 결과
+  //   const api_url = 'https://openapi.naver.com/v1/search/blog.xml?query=' + encodeURI(req.query.query); // XML 결과
+  const request = require("request");
+  const options = {
+    url: api_url,
+    headers: { "X-Naver-Client-Id": client_id, "X-Naver-Client-Secret": client_secret },
+  };
+  request.get(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.writeHead(200, { "Content-Type": "text/json;charset=utf-8" });
+      res.end(body);
+    } else {
+      res.status(response.statusCode).end();
+      console.log("error = " + response.statusCode);
+    }
+  });
+});
+app.listen(3000, function () {
+  console.log("http://127.0.0.1:3000/search/blog?query=검색어 app listening on port 3000!");
+});
